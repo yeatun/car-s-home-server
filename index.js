cb1d894e984fbf8c1a62fac500ae34f41d700938
unknown <p.yeatun@gmail.com>
2021-04-17 23:51:13 +0600

    updated

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const serviceCollection = client.db("carService").collection("services");
   const adminCollection = client.db("carService").collection("admin");
+  const addReviewCollection = client.db("carService").collection("addReview");
+  const newServiceCollection = client.db("carService").collection("addNewService");
   // perform actions on the collection object
 
   //service
@@ -35,6 +37,15 @@ client.connect(err => {
             res.send(result.insertedCount > 0)
         })
 });
+// app.post('/addService', (req, res) => {
+//     const queryEmail = req.query.email;
+//     serviceCollection.find({email: queryEmail})
+//     .toArray((err, documents) => {
+//       res.send(documents)
+//       console.log(documents)
+//       console.log(err);
+//     })
+//   })
 app.post('/orderCollection', (req, res) => {
   const date = req.body;
   const email = req.body.email;
@@ -50,16 +61,7 @@ app.post('/orderCollection', (req, res) => {
               res.send(documents);
           })
       })
-      // {
-      //   const queryEmail = req.query.email;
-      //   ordersCollection.find({email: queryEmail})
-      //   .toArray((err, documents) => {
-      //     res.send(documents)
-      //     console.log(documents)
-      //     console.log(err);
-      //   })
-      // }
- 
+  
 });
 app.get('/serviceData', (req, res) => {
     serviceCollection.find({})
@@ -82,53 +84,82 @@ app.post('/serviceByDate', (req, res) => {
                     res.send(documents);
                 })
         })
-//     const date = req.body;
-//     console.log(date.date);
-//   serviceCollection.find({date:date.date})
-//       .toArray((err, documents) => {
-//           res.send(documents);
-//       })
+
 })
 app.post('/addAdmin', (req, res) => {
-//   const file = req.files.file;
-//   const name = req.body.name;
-  const email = req.body.email;
-  console.log(email)
-//   const newImg = file.data;
-//   const encImg = newImg.toString('base64');
+    //   const file = req.files.file;
+    //   const name = req.body.name;
+      const email = req.body.email;
+      console.log(email)
+    //   const newImg = file.data;
+    //   const encImg = newImg.toString('base64');
+    
+    //   var image = {
+    //       contentType: file.mimetype,
+    //       size: file.size,
+    //       img: Buffer.from(encImg, 'base64')
+    //   };
+    
+      adminCollection.insertOne({ email})
+          .then(result => {
+              res.send(result.insertedCount > 0);
+          })
+    })
 
-//   var image = {
-//       contentType: file.mimetype,
-//       size: file.size,
-//       img: Buffer.from(encImg, 'base64')
-//   };
-
-  adminCollection.insertOne({ email})
-      .then(result => {
-          res.send(result.insertedCount > 0);
-      })
-})
 app.post('/addReview', (req, res) => {
     const newEvent = req.body;
     console.log('adding new event: ', newEvent)
-    serviceCollection.insertOne(newEvent)
+    addReviewCollection.insertOne(newEvent)
+    .then(result => {
+        console.log('inserted count', result.insertedCount);
+        res.send(result.insertedCount > 0)
+    })
+})
+app.post('/addNewService', (req, res) => {
+    const newEvent = req.body;
+    console.log('adding new event: ', newEvent)
+    newServiceCollection.insertOne(newEvent)
     .then(result => {
         console.log('inserted count', result.insertedCount);
         res.send(result.insertedCount > 0)
     })
 })
 app.get('/review', (req, res) => {
-    serviceCollection.find()
+    addReviewCollection.find()
     .toArray((err, items) => {
         res.send(items)
     })
 })
+app.get('/order', (req, res) => {
+    const queryEmail = req.query.email;
+    serviceCollection.find({email: queryEmail})
+    .toArray((err, documents) => {
+      res.send(documents)
+      console.log(documents)
+      console.log(err);
+    })
+  })
+app.get('/newService', (req, res) => {
+    newServiceCollection.find()
+    .toArray((err, items) => {
+        res.send(items)
+    })
+})
+
 app.post('/isAdmin', (req, res) => {
   const email = req.body.email;
   adminCollection.find({ email: email })
       .toArray((err, admins) => {
           res.send(admins.length > 0);
       })
+})
+app.delete('/delete', (req, res) => {
+    const id = ObjectId (req.params._id);
+    console.log('deleted id', id)
+    adminCollection.findOneAndDelete({_id: id})
+    .then(result => 
+        {result.deletedCount > 0})
+        
 })
 
 
